6025c70353831fdc9bc223e10ad05470b753897b
Anya <likamoon@list.ru>
2020-08-20 01:42:10 +0300

    Календарь datapicker

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,71 @@
-$(document).ready(function () {    
+$(document).ready(function () {
+
+    var hasCalendar = document.querySelector('.calendar');
+    if (hasCalendar) {
+
+        $.datepicker.regional['ru'] = {
+            closeText: 'Закрыть',
+            prevText: 'Предыдущий',
+            nextText: 'Следующий',
+            currentText: 'Сегодня',
+            monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+            monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+            dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+            dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+            dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+            weekHeader: 'Не',
+            dateFormat: 'dd.mm.yy',
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: ''
+        };
+        $.datepicker.setDefaults($.datepicker.regional['ru']);
+
+        var currentDate = 0; //переменная для хранения выбранной даты
+        var currentDays = ["2020-8-21", "2020-8-24", "2020-8-27", "2020-8-7", "2020-8-11", "2020-8-28", "2020-8-1", "2020-7-30", "2020-9-5"]; //массив "активных" дат
+        var datepicker = $("#datepicker").datepicker({
+            showOtherMonths: true,
+            selectOtherMonths: true,
+            // minDate: 0,
+            onSelect: function (date) { //при изменении даты
+                $('#datepicker_value').val(date);
+                currentDate = date;
+                console.log(currentDate);
+            },
+            beforeShowDay: function (date) {
+                var m = date.getMonth();
+                var d = date.getDate();
+                var y = date.getFullYear();
+
+                var today, yesterday;
+                today = new Date();
+                yesterday = new Date(today.setUTCDate(today.getUTCDate() - 1));
+
+                for (var i = 0; i < currentDays.length; i++) {
+                    if ($.inArray(y + '-' + (m + 1) + '-' + d, currentDays) !== -1) {
+                        if (date < yesterday) {
+                            return [true, 'calendar-before calendar-active', ''];
+                        } else {
+                            return [true, 'calendar-active', ''];
+                        }
+                    } else {
+                        if (date < yesterday) {
+                            return [true, 'calendar-before', ''];
+                            // return [false, 'calendar-before', ''];
+                        }
+                        else {
+                            // return [false, "", ""];
+                        }
+                    }
+                }
+                return [true];
+            }
+        });
+        currentDate = datepicker[0].value;
+        console.log(currentDate);
+    }
+
 
 })
 
